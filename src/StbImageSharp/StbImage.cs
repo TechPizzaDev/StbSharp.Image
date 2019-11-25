a7eb9969fb117b4a0d23cb837347ce482ad5ea06
Michal Piatkowski <piatkowski2002@gmail.com>
2019-11-25 19:27:41 +0100

    Rename 'Cancellation' field

--- src/StbImageSharp/StbImage.cs
+++ src/StbImageSharp/StbImage.cs
@@ -6,8 +6,8 @@ using System.Threading;
 namespace StbSharp
 {
     public static unsafe partial class StbImage
-	{
-		public static string LastError;
+    {
+        public static string LastError;
         public static int stbi__vertically_flip_on_load;
 
         public delegate int ReadCallback(ReadContext context, Span<byte> data);
@@ -18,7 +18,7 @@ namespace StbSharp
         {
             public readonly Stream Stream;
             public readonly byte[] ReadBuffer;
-            public readonly CancellationToken Cancellation;
+            public readonly CancellationToken CancellationToken;
 
             public readonly ReadCallback Read;
             public readonly SkipCallback Skip;
@@ -41,7 +41,7 @@ namespace StbSharp
                 ReadFromCallbacks = false;
                 Read = null;
                 Skip = null;
-                Cancellation = cancellation;
+                CancellationToken = cancellation;
 
                 DataLength = len;
                 DataStart = null;
@@ -55,7 +55,7 @@ namespace StbSharp
             {
                 Stream = stream;
                 ReadBuffer = readBuffer;
-                Cancellation = cancellation;
+                CancellationToken = cancellation;
 
                 Read = read;
                 Skip = skip;
@@ -110,79 +110,79 @@ namespace StbSharp
             return 0;
         }
 
-		public delegate void IdctBlockKernel(byte* output, int out_stride, short* data);
+        public delegate void IdctBlockKernel(byte* output, int out_stride, short* data);
 
-		public delegate void YCbCrToRgbKernel(
-			byte* output, byte* y, byte* pcb, byte* pcr, int count, int step);
+        public delegate void YCbCrToRgbKernel(
+            byte* output, byte* y, byte* pcb, byte* pcr, int count, int step);
 
-		public delegate byte* ResamplerMethod(byte* a, byte* b, byte* c, int d, int e);
+        public delegate byte* ResamplerMethod(byte* a, byte* b, byte* c, int d, int e);
 
         private static readonly IdctBlockKernel _cached__idct_block = stbi__idct_block;
         private static readonly YCbCrToRgbKernel _cached__YCbCr_to_RGB_row = stbi__YCbCr_to_RGB_row;
         private static readonly ResamplerMethod _cached__resample_row_hv_2 = stbi__resample_row_hv_2;
 
-		[StructLayout(LayoutKind.Sequential)]
-		public struct JpegComponent
-		{
-			public int id;
-			public int h, v;
-			public int tq;
-			public int hd, ha;
-			public int dc_pred;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct JpegComponent
+        {
+            public int id;
+            public int h, v;
+            public int tq;
+            public int hd, ha;
+            public int dc_pred;
 
-			public int x, y, w2, h2;
-			public byte* data;
-			public void* raw_data;
-			public void* raw_coeff;
-			public byte* linebuf;
-			public short* coeff; // progressive only
-			public int coeff_w, coeff_h; // number of 8x8 coefficient blocks
-		}
+            public int x, y, w2, h2;
+            public byte* data;
+            public void* raw_data;
+            public void* raw_coeff;
+            public byte* linebuf;
+            public short* coeff; // progressive only
+            public int coeff_w, coeff_h; // number of 8x8 coefficient blocks
+        }
 
-		public class JpegContext
+        public class JpegContext
         {
             public const int STBI__ZFAST_BITS = 9;
 
             public ReadContext s;
-			public readonly stbi__huffman[] huff_dc = new stbi__huffman[4];
-			public readonly stbi__huffman[] huff_ac = new stbi__huffman[4];
-			public readonly ushort[][] dequant;
+            public readonly stbi__huffman[] huff_dc = new stbi__huffman[4];
+            public readonly stbi__huffman[] huff_ac = new stbi__huffman[4];
+            public readonly ushort[][] dequant;
 
-			public readonly short[][] fast_ac;
+            public readonly short[][] fast_ac;
 
             // sizes for components, interleaved MCUs
-			public int img_h_max, img_v_max;
-			public int img_mcu_x, img_mcu_y;
-			public int img_mcu_w, img_mcu_h;
+            public int img_h_max, img_v_max;
+            public int img_mcu_x, img_mcu_y;
+            public int img_mcu_w, img_mcu_h;
 
             // definition of jpeg image component
-			public JpegComponent[] img_comp = new JpegComponent[4];
+            public JpegComponent[] img_comp = new JpegComponent[4];
 
-			public uint code_buffer; // jpeg entropy-coded buffer
-			public int code_bits; // number of valid bits
-			public byte marker; // marker seen while filling entropy buffer
-			public int nomore; // flag if we saw a marker so must stop
+            public uint code_buffer; // jpeg entropy-coded buffer
+            public int code_bits; // number of valid bits
+            public byte marker; // marker seen while filling entropy buffer
+            public int nomore; // flag if we saw a marker so must stop
 
-			public int progressive;
-			public int spec_start;
-			public int spec_end;
-			public int succ_high;
-			public int succ_low;
-			public int eob_run;
-			public int jfif;
-			public int app14_color_transform; // Adobe APP14 tag
-			public int rgb;
+            public int progressive;
+            public int spec_start;
+            public int spec_end;
+            public int succ_high;
+            public int succ_low;
+            public int eob_run;
+            public int jfif;
+            public int app14_color_transform; // Adobe APP14 tag
+            public int rgb;
 
-			public int scan_n;
-			public int[] order = new int[4];
-			public int restart_interval, todo;
+            public int scan_n;
+            public int[] order = new int[4];
+            public int restart_interval, todo;
 
-			public IdctBlockKernel idct_block_kernel;
-			public YCbCrToRgbKernel YCbCr_to_RGB_kernel;
-			public ResamplerMethod resample_row_hv_2_kernel;
+            public IdctBlockKernel idct_block_kernel;
+            public YCbCrToRgbKernel YCbCr_to_RGB_kernel;
+            public ResamplerMethod resample_row_hv_2_kernel;
 
             public JpegContext(ReadContext ctx)
-			{
+            {
                 s = ctx;
 
                 idct_block_kernel = _cached__idct_block;
@@ -190,43 +190,43 @@ namespace StbSharp
                 resample_row_hv_2_kernel = _cached__resample_row_hv_2;
 
                 for (var i = 0; i < 4; ++i)
-				{
-					huff_ac[i] = new stbi__huffman();
-					huff_dc[i] = new stbi__huffman();
-				}
+                {
+                    huff_ac[i] = new stbi__huffman();
+                    huff_dc[i] = new stbi__huffman();
+                }
 
-				for (var i = 0; i < img_comp.Length; ++i)
-					img_comp[i] = new JpegComponent();
+                for (var i = 0; i < img_comp.Length; ++i)
+                    img_comp[i] = new JpegComponent();
 
-				fast_ac = new short[4][];
-				for (var i = 0; i < fast_ac.Length; ++i)
-					fast_ac[i] = new short[1 << STBI__ZFAST_BITS];
+                fast_ac = new short[4][];
+                for (var i = 0; i < fast_ac.Length; ++i)
+                    fast_ac[i] = new short[1 << STBI__ZFAST_BITS];
 
-				dequant = new ushort [4][];
-				for (var i = 0; i < dequant.Length; ++i)
-					dequant[i] = new ushort[64];
-			}
-		};
+                dequant = new ushort [4][];
+                for (var i = 0; i < dequant.Length; ++i)
+                    dequant[i] = new ushort[64];
+            }
+        };
 
-		public struct stbi__resample
-		{
-			public ResamplerMethod Resample;
-			public byte* line0;
-			public byte* line1;
-			public int hs;
-			public int vs;
-			public int w_lores;
-			public int ystep;
-			public int ypos;
+        public struct stbi__resample
+        {
+            public ResamplerMethod Resample;
+            public byte* line0;
+            public byte* line1;
+            public int hs;
+            public int vs;
+            public int w_lores;
+            public int ystep;
+            public int ypos;
         }
 
-		[StructLayout(LayoutKind.Sequential)]
-		public struct GifLzw
-		{
-			public short prefix;
-			public byte first;
-			public byte suffix;
-		}
+        [StructLayout(LayoutKind.Sequential)]
+        public struct GifLzw
+        {
+            public short prefix;
+            public byte first;
+            public byte suffix;
+        }
 
         public struct GifContext
         {
@@ -294,5 +294,5 @@ namespace StbSharp
                 }
             }
         }
-	}
+    }
 }
